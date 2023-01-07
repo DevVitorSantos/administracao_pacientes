9653189b8da29a2f192b4d4b4d61f2f35353ba45
Vitor Santos <vitorsantos.seo@gmail.com>
2023-01-07 19:15:51 -0300

    mudanca no titulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ const Header = () => {
   return ( 
     <>
       <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-        Seguimentos Pacientes{' '}
-        <span className="text-indigo-600">Veterinaria</span></h1>
+        Seguimentos Pacientes{' '} Com Githut {' '}
+        <span className="text-indigo-600">Veterinario</span></h1>
     </>
    )
 }
